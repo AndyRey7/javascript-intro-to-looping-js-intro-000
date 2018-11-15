9cf5cfc9ce03bb6685ac50a237aea86856ce5fb2
andy reyes <11710reyes@gmail.com>
2018-11-15 03:47:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,14 +11,6 @@ function forLoop(array) {
 
 function whileLoop(n) {
   let countdown = n;
-  while (countdown > 0) {
-    console.log(--countdown)
-    return 'done'
-  }
-}
-
-	function whileLoop(n) {
-  let countdown = n;
      while (countdown > 0) {
       console.log(--countdown)
     }
