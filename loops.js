c3b0aadddcfa24fb33492ba03e44520a41d9c508
andy reyes <11710reyes@gmail.com>
2018-11-15 03:55:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,10 +18,11 @@ function whileLoop(n) {
   }
   
   function doWhileLoop(array) {
-    function maybeTrue() {
-  return Math.random() >= 0.5;
-    }
-    do {
-      
-    }
+  function maybeTrue() {
+    return Math.random() >= 0.5
   }
+   do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
+}
